ea5fc8181aab6b81919c5cb5900a33f9b977d656
Mantas Marcinkevičius <marc.mantas@gmail.com>
2016-09-07 11:50:19 +0300

    added a filter funcitonal test

---
+++ Tests/Functional/Filter/Widget/Dynamic/DynamicAggregateFilterTest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Tests\Functional\Filter\Widget\Dynamic;
+
+use ONGR\ElasticsearchBundle\Test\AbstractElasticsearchTestCase;
+
+class DynamicAggregateFilterTest extends AbstractElasticsearchTestCase
+{
+
+}
