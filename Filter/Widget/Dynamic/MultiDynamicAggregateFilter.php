3c49b6d68c6a6d16c661a73828b1cdd2473664ff
Mantas Marcinkevičius <marc.mantas@gmail.com>
2016-09-09 10:57:01 +0300

    extended modifySearch method to filter on multiple selections

--- Filter/Widget/Dynamic/MultiDynamicAggregateFilter.php
+++ Filter/Widget/Dynamic/MultiDynamicAggregateFilter.php
@@ -2,7 +2,69 @@
 
 namespace ONGR\FilterManagerBundle\Filter\Widget\Dynamic;
 
+use ONGR\ElasticsearchBundle\Result\Aggregation\AggregationValue;
+use ONGR\ElasticsearchBundle\Result\DocumentIterator;
+use ONGR\ElasticsearchDSL\Aggregation\FilterAggregation;
+use ONGR\ElasticsearchDSL\Aggregation\NestedAggregation;
+use ONGR\ElasticsearchDSL\Aggregation\TermsAggregation;
+use ONGR\ElasticsearchDSL\BuilderInterface;
+use ONGR\ElasticsearchDSL\Query\BoolQuery;
+use ONGR\ElasticsearchDSL\Query\MatchAllQuery;
+use ONGR\ElasticsearchDSL\Query\NestedQuery;
+use ONGR\ElasticsearchDSL\Query\TermQuery;
+use ONGR\ElasticsearchDSL\Query\TermsQuery;
+use ONGR\ElasticsearchDSL\Search;
+use ONGR\FilterManagerBundle\Filter\FilterState;
+use ONGR\FilterManagerBundle\Filter\ViewData;
+use ONGR\FilterManagerBundle\Search\SearchRequest;
+use Symfony\Component\HttpFoundation\Request;
+
 class MultiDynamicAggregateFilter extends DynamicAggregateFilter
 {
-    
+    /**
+     * {@inheritdoc}
+     */
+    public function getState(Request $request)
+    {
+        $state = new FilterState();
+        $value = $request->get($this->getRequestField());
+
+        if (isset($value) && is_array($value) && reset($value) && is_array(reset($value))) {
+            $state->setActive(true);
+            $state->setValue($value);
+            $state->setUrlParameters([$this->getRequestField() => $value]);
+        }
+
+        return $state;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function modifySearch(Search $search, FilterState $state = null, SearchRequest $request = null)
+    {
+        list($path, $field) = explode('>', $this->getField());
+
+        if ($state && $state->isActive()) {
+            $boolQuery = new BoolQuery();
+
+            foreach ($state->getValue() as $groupName => $values) {
+                $innerBoolQuery = new BoolQuery();
+
+                foreach ($values as $value) {
+                    $innerBoolQuery->add(
+                        new NestedQuery(
+                            $path,
+                            new TermQuery($field, $value)
+                        ),
+                        BoolQuery::SHOULD
+                    );
+                }
+
+                $boolQuery->add($innerBoolQuery);
+            }
+
+            $search->addPostFilter($boolQuery);
+        }
+    }
 }
