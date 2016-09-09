29c78bc401af66fe5d0b176108642f4eff96f2fa
Mantas Marcinkevičius <marc.mantas@gmail.com>
2016-09-09 11:00:05 +0300

    extended formation methods in dynamicAggregateFilter

--- Filter/Widget/Dynamic/MultiDynamicAggregateFilter.php
+++ Filter/Widget/Dynamic/MultiDynamicAggregateFilter.php
@@ -67,4 +67,121 @@ class MultiDynamicAggregateFilter extends DynamicAggregateFilter
             $search->addPostFilter($boolQuery);
         }
     }
+
+    /**
+     * Fetches buckets from search results.
+     *
+     * @param DocumentIterator $result     Search results.
+     * @param string           $filterName Filter name.
+     * @param array            $values     Values from the state object
+     *
+     * @return array Buckets.
+     */
+    protected function fetchAggregation(DocumentIterator $result, $filterName, $values)
+    {
+        $data = [];
+        $aggregation = $result->getAggregation(sprintf('%s-filter', $filterName));
+
+        if ($aggregation->getAggregation($filterName)) {
+            $aggregation = $aggregation->find($filterName.'.query');
+            $data['all-selected'] = $aggregation;
+
+            return $data;
+        }
+
+        if (!empty($values)) {
+            foreach ($values as $name => $value) {
+                $data[$name] = $aggregation->find(sprintf('%s.%s.query', $name, $filterName));
+            }
+
+            $data['all-selected'] = $aggregation->find(sprintf('all-selected.%s.query', $filterName));
+
+            return $data;
+        }
+
+        return [];
+    }
+
+    /**
+     * A method used to add an additional filter to the aggregations
+     * in preProcessSearch
+     *
+     * @param FilterAggregation $filterAggregation
+     * @param NestedAggregation $deepLevelAggregation
+     * @param array             $terms Terms of additional filter
+     * @param string            $aggName
+     *
+     * @return BuilderInterface
+     */
+    protected function addSubFilterAggregation(
+        $filterAggregation,
+        $deepLevelAggregation,
+        $terms,
+        $aggName
+    ) {
+        list($path, $field) = explode('>', $this->getField());
+        $boolQuery = new BoolQuery();
+
+        foreach ($terms as $namedTerms) {
+            $boolQuery->add(
+                new NestedQuery($path, new TermsQuery($field, array_values($namedTerms)))
+            );
+        }
+
+        if ($boolQuery->getQueries() == []) {
+            $boolQuery->add(new MatchAllQuery());
+        }
+
+        $innerFilterAggregation = new FilterAggregation(
+            $aggName,
+            $boolQuery
+        );
+        $innerFilterAggregation->addAggregation($deepLevelAggregation);
+        $filterAggregation->addAggregation($innerFilterAggregation);
+    }
+
+    /**
+     * @param string   $key
+     * @param string   $name
+     * @param ViewData $data
+     * @param bool     $active True when the choice is active
+     *
+     * @return array
+     */
+    protected function getOptionUrlParameters($key, $name, ViewData $data, $active)
+    {
+        $value = $data->getState()->getValue();
+        $parameters = $data->getResetUrlParameters();
+
+        if (!empty($value)) {
+            if ($active) {
+                unset($value[$name][array_search($key, $value[$name])]);
+                $parameters[$this->getRequestField()] = $value;
+
+                return $parameters;
+            }
+
+            $parameters[$this->getRequestField()] = $value;
+        }
+
+        $parameters[$this->getRequestField()][$name][] = $key;
+
+        return $parameters;
+    }
+
+    /**
+     * Returns whether choice with the specified key is active.
+     *
+     * @param string   $key
+     * @param ViewData $data
+     * @param string   $activeName
+     *
+     * @return bool
+     */
+    protected function isChoiceActive($key, ViewData $data, $activeName)
+    {
+        return $data->getState()->isActive() &&
+            isset($data->getState()->getValue()[$activeName]) &&
+            in_array($key, $data->getState()->getValue()[$activeName]);
+    }
 }
