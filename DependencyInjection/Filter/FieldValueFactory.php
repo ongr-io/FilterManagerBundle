80ff75bc8f97a0eedaf9e6e2ea404d37a1687bd3
Simonas Šerlinskas <simonas.serlinskas@nfq.com>
2016-10-12 15:00:36 +0300

    added deprecations

--- DependencyInjection/Filter/FieldValueFactory.php
+++ DependencyInjection/Filter/FieldValueFactory.php
@@ -15,6 +15,8 @@ use Symfony\Component\DependencyInjection\Definition;
 
 /**
  * Factory for field_value factory.
+ *
+ * @deprecated Filter factories will be deleted in 2.0
  */
 class FieldValueFactory extends AbstractFilterFactory
 {
